28a6e809da253823dc545ec5d6579fec0bfa4f0e
KennedyLins <kennedy.lins@gmail.com>
2021-04-14 23:35:33 -0300

    Ajustes exibição dos dados

--- routes/web.php
+++ routes/web.php
@@ -2,30 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/new', function () {
-    return view('monitor2');
-});
 
 Route::get('/', 'MainController@index');
 
 Auth::routes();
 
-
-Route::get('/station', 'StationController@index');
-
-Route::get('/addStation', 'StationController@create');
-
-Route::get('/saveStation', 'StationController@store');
-
-Route::get('/home', 'HomeController@index')->name('home');
