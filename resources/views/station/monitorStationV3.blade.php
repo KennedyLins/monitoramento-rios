2d5f2f7aced22721899456f90fe4acd4ae3689d9
KennedyLins <kennedy.lins@gmail.com>
2021-04-15 18:06:32 -0300

    Inicio de configuração dos gráficos

--- resources/views/station/monitorStationV3.blade.php
+++ resources/views/station/monitorStationV3.blade.php
@@ -10,27 +10,17 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="content-language" content="pt-BR">
     
-  <!--Refresh Automatico da Pagina a cada 15 minutos (900 segundos)       TituloGrid  -->
+  <!--Refresh Automatico da Pagina a cada 15 minutos (900 segundos)-->
   <meta http-equiv="refresh" content="900"> 
 
-<link rel="stylesheet" https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css>
+<!--<link rel="stylesheet" https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css>-->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <link rel="stylesheet" href="{{asset('css/monitoramento-pluviometrosV3.css')}}" type="text/css">
 <link rel="stylesheet" https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-<style>
-	.colorFlood{background-color: darkorange;}
-	.colorAlert{background-color: yellow;}
-	.colorPreAlert{background-color: lightyellow;}
-	.legenda{
 
-		align-self: right;
-		border: solid 1.5px #c9cdca;
-		width: 15%;
-		height: 70%
-		text-align: center;
-	}
 
-</style>
 
 </head>
 
@@ -41,18 +31,20 @@
 				<img src="{{asset('img/tira-apac-ana-governo.png')}}">
 			</div>
 
-			<div class="row border titulo">
-			<h2>Monitoramento dos Rios do Estado de Pernambuco - v2</h2>
-			<h2>Rede de Alerta<h2>
+			<div class="row ">
+				<h4>Monitoramento dos Rios do Estado de Pernambuco - v2</h4>
+				<div class="row ">
+				<h4>Rede de Alerta</h4>
+				</div>
 			</div>
 			<div class="row border time">
-			<h2>Informações atualizadas em : <?php echo date('d M Y') . " às " . date('H:i:s')?></h2>
+				<h6>Informações atualizadas em : <?php echo date('d M Y') . " às " . date('H:i:s')?></h6>
 			
 			</div>
 			<div class="legenda">
-				<h3 class="colorPreAlert">Pre-alerta</h3>
-				<h3 class="colorAlert">Alerta</h3>
-				<h3 class="colorFlood">Inundação</h3>
+				<p class="colorPreAlert">Pre-alerta</p>
+				<p class="colorAlert">Alerta</p>
+				<p class="colorFlood">Inundação</p>
 			</div>
 
 			<div class="listaRios">
@@ -84,10 +76,11 @@
 					      <td class="gridDados">{{$hidro_station->levelNow}}</td>
 					      <td class="gridDados">{{$hidro_station->alertLevel}}</td>
 					      <td class="gridDados">{{$hidro_station->floodLevel}}</td>
-						  <td class="gridDados">-</td>							
+						  <td ><button class="btn" data-toggle="modal" data-target="#meuModal" v-on:click="showGraph({{$hidro_station}})"><i class="fa fa-search"></i></button></td>							
 						</tr>									
 				     @endforeach
-				     
+
+
 				  </tbody>
 				</table>
 			</div>
@@ -95,10 +88,45 @@
 		</div>
 	</div>
 
+<!--Modal -->
+<div id="meuModal" class="modal fade" role="dialog">
+      <div class="modal-dialog">
+
+        <!-- Conteúdo do modal-->
+        <div id="modalContent"class="modal-content">
+
+          <!-- Cabeçalho do modal -->
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal">&times;</button>
+            </div>
+
+            <!-- Corpo do modal -->
+            <div id="modalbody" class="modal-body">
+				<div id='conteudomodal'></div>
+              
+            </div>
+
+            <!-- Rodapé do modal-->
+            <div class="modal-footer">
+               <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+    
+			</div>
+
+        </div>
+      </div>
+    </div>
+
+<!-- Fim Modal -->
+
 <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script type="text/javascript" src="https://www.google.com/jsapi"></script>
 
+
+<!--Trabalhando com Vue js para alterar as cores dos alertas -->
 <script>
-
 
 new Vue({		
 
@@ -106,7 +134,7 @@ new Vue({
 
 	data: {		
 
-		defineClass: ['gridDados','colorPreAlert','colorAlert','colorFlood']
+		defineClass: ['gridDados','colorPreAlert','colorAlert','colorFlood'],
 
 	},
 	methods: {
@@ -128,11 +156,62 @@ new Vue({
 					return this.defineClass[0]
 				}
 			}			
-		}		 
+		},
+
+		showGraph (dados2){
+
+			return console.log(dados2.idStation)
+		}	 
 	}
 })
 
 </script>
+
+
+
+<!--Trabalhando com google chart para gerar os gráficos -->
+<script>
+
+   google.load("visualization", "1", {packages:["corechart"]});
+      
+   google.setOnLoadCallback(drawChart);
+      
+      
+      function drawChart() {
+
+    //montando o array com os dados
+        var data = google.visualization.arrayToDataTable([
+          ['Hora', 'Nível'],
+          ['18:00',  67.90],
+          ['18:30',  62.90],
+          ['19:00',  75.40],
+          ['19:30',  72.90],
+          ['20:00',  70.50],
+          ['20:30',  67.90],
+          ['21:00',  65.12],
+          ['21:30',  59.90],
+          ['22:00',  62.17],
+         
+        ]);
+        
+    //opções para o gráfico linhas
+    var options = {
+          title: 'Nível do Rio',
+
+          //legenda na horizontal
+          hAxis: {
+          	title: 'Data/Hora',  
+          	titleTextStyle: {color: 'blue'}},
+          width: 450,
+          height: 200	
+        };
+
+    //instanciando e desenhando o gráfico linhas
+        var linhas = new google.visualization.LineChart(document.getElementById('conteudomodal'));
+        linhas.draw(data, options);
+
+      }
+    </script>
 
 </body>
 
